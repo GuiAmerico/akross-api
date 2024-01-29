ca4bfeac65ea5c7fde9fd729cdca7852d83c81ee
GuiAmerico <guilhermao127@gmail.com>
2024-01-29 15:56:12 -0300

    :tada: init

---
+++ src/main/java/br/com/akross/AkrossAPI/AkrossApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.akross.AkrossAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkrossApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkrossApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/akross/AkrossAPI/AkrossApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.akross.AkrossAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkrossApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
